f9ad0153825014b0d45ea86eee47938d179c2d64
Oliver Derafera <oliverderafera@gmail.com>
2013-11-29 14:46:13 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mailit::Application.config.secret_key_base = '4cfc2cde6d6cd45918d9c566b4256f4b62b75297b4cecdb71b02e8deaea8460f7f398c058d57611be0e9d6028e304a38cab2ebe8695b887cd7dd9427a22d4bb2'
