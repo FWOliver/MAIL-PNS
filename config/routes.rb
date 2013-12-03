0030ba7e44f77042203b310af807f94a4506ef02
Oliver Derafera <oliverderafera@gmail.com>
2013-12-03 13:39:23 +0000

    other files created by devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Mailit::Application.routes.draw do
-  resources :users
 
+  devise_for :models
+	devise_for :users
+  
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
